abe3404f9de3b0a11a6b7390c2b6232076691859
Aaron <astrauli@gmail.com>
2017-04-29 07:12:15 -0700

    Name all methods

---
+++ linkedLists.rb
@@ -0,0 +1,48 @@
+class LinkedList
+  def initialize
+  end
+
+  def append
+  end
+
+  def prepend
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?
+  end
+
+  def find(data)
+  end
+
+  def to_s
+  end
+  
+end
+
+class Node
+  attr_accessor :value,
+
+  def value(value = nil)
+    @value = value
+  end
+
+  def next_node
+
+  end
+
+end
